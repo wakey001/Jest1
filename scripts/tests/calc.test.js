a4c2dc7d68ee0eb72dffce74183f73878b4ce487
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-04-30 23:24:04 +0000

    added to function addition

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,10 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
 const addition = require("../calc");
 
 describe("calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
-            expect(addition(20,22)).toBe(42);
-        })
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
     });
     describe("subtraction function", () => {
 
