c4d1988b2fa86dbf38894d3eff9560c71e982834
wakey001 <stefanwakeman001@hotmail.co.uk>
2022-05-02 20:50:22 +0000

    finished test walkthrough

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,21 @@
+
+const buttonClick = require("../button");
+
+beforeEach( () => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe[1];
+
+    })
+})
